cb133857a1d6bb7766521165849a7d0b00429b43
didoguan <didoguan@163.com>
2020-11-17 22:41:16 +0800

    修改配置

--- manager/src/main/java/com/deepspc/manager/conf/MybatisPlusConfig.java
+++ manager/src/main/java/com/deepspc/manager/conf/MybatisPlusConfig.java
@@ -3,6 +3,7 @@ package com.deepspc.manager.conf;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -15,16 +16,19 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableTransactionManagement
 @Configuration
-@MapperScan("com.deepspc.**.mapper")
+@MapperScan("com.deepspc.manager.*.mapper")
 public class MybatisPlusConfig {
 
   /**
-   * 分页插件
+   * 插件
    * @return
    */
   @Bean
   public MybatisPlusInterceptor mybatisPlusInterceptor() {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    // 乐观锁
+	interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+	// 分页
     interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
     return interceptor;
   }
