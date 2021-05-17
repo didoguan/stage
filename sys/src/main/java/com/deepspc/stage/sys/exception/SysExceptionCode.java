96e38990e88d82106ef6b0791f9014c12f0ea687
didoguan <didoguan@163.com>
2021-05-17 17:57:51 +0800

    修改数据模型

--- sys/src/main/java/com/deepspc/stage/sys/exception/SysExceptionCode.java
+++ sys/src/main/java/com/deepspc/stage/sys/exception/SysExceptionCode.java
@@ -9,7 +9,7 @@ public enum SysExceptionCode {
     PARAM_REQUIRE("710", "请求参数不能为空"),
     USER_EXISTS("711", "该用户已经登录"),
     CAPTCHACODE_NULL("712", "验证码不能为空"),
-    CAPTCHACODE_NOT_MATCH("713", "验证码不正确"),
+    CAPTCHACODE_NOT_MATCH("713", "验证码失效或不正确"),
     FILE_UPLOAD_EMPTY("714", "要上传的文件为空"),
     FILE_UPLOAD_FAILED("715", "文件上传失败"),
 
